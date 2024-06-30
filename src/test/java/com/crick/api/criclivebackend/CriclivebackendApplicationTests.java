c076e07680c573b565178b4fef397fb0e0638e62
adityasachan24@gmail.com <adityasachan24@gmail.com>
2024-06-30 14:33:12 +0530

    first commit

---
+++ src/test/java/com/crick/api/criclivebackend/CriclivebackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crick.api.criclivebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CriclivebackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
