3345dd2a1d570b0dbd0b24f8c42dddbfbc6b2542
adityasachan24@gmail.com <adityasachan24@gmail.com>
2024-07-02 21:06:55 +0530

    added required fileS

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,31 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import { HomeComponent } from '../../pages/home/home.component';
+import { HistoryComponent } from '../../pages/history/history.component';
+import { LiveComponent } from '../../pages/live/live.component';
+import { PointsTableComponent } from '../../pages/points-table/points-table.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+ path:"home",
+ component:HomeComponent,
+ title:"Home Page | CrickLive"
+
+    },
+    {
+      path: 'history',
+      component:HistoryComponent,
+      title:"Match History | CrickLive"
+    },
+    {
+        path: 'live',
+        component:LiveComponent,
+        title:"Live Matches | CrickLive"
+
+    },
+    {
+        path: 'points-table',
+        component:PointsTableComponent,
+        title:"PointTable | CrickLive"
+    }
+];
