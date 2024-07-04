73c97e17165f79a53413d88f987564ff56786fa7
adityasachan24@gmail.com <adityasachan24@gmail.com>
2024-07-04 16:24:30 +0530

    files in a way

---
+++ src/app/live/live.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiCallService } from '../services/api-call.service';
+import { CommonModule, JsonPipe } from '@angular/common';
+
+@Component({
+  selector: 'app-live',
+  standalone: true,
+  imports: [JsonPipe,CommonModule],
+  templateUrl: './live.component.html',
+  styleUrl: './live.component.css'
+})
+export class LiveComponent implements OnInit {
+  livematches:any
+  constructor (private api:ApiCallService)
+  {
+    
+  }
+  ngOnInit(): void {
+    this.livemethod();
+  }
+
+
+  private livemethod() {
+    this.api.getLiveMatches().subscribe({
+      next: data => {
+        console.log(data);
+        this.livematches=data;
+      },
+      error: err => {
+        console.log(err);
+      }
+
+    });
+  }
+}
